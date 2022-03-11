10beb5cbc957687fd83956635100ed4f514d4dbf
Dmitrio <ar-dimasik@ya.ru>
2022-03-11 11:28:24 +0100

    HomeWork Unit 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function sortByHealth(arr) {
+    arr.sort((a, b) => (a.health > b.health ? -1 : 1));
+  }
+  
+  export const characters = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
